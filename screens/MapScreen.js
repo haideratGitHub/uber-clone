3e227380d5e068764bef6e3cfc7e6b5f44fac2fb
haiderAtGitHub <haiderabbas1982000@gmail.com>
2021-08-16 19:07:13 +0500

    Implemented navigation

---
+++ screens/MapScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+
+const MapScreen = () => {
+	return (
+		<SafeAreaView>
+			<Text>Map Screen</Text>
+		</SafeAreaView>
+	);
+};
+
+export default MapScreen;
+
+const styles = StyleSheet.create({});
